748c6f8fba7e9ce4e274d3a990358e20b9d40418
Marco <bigdatajang@gmail.com>
2020-01-19 23:33:11 +0900

    Updates

---
+++ precache-manifest.f4d333cc984b4d4ca1672ab40bec3312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7400e3c4f275f9621a4c7f865cb4628",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b2e9f07d70cdf6934d86",
+    "url": "/movie_app/static/css/main.9de8ace0.chunk.css"
+  },
+  {
+    "revision": "3c4b4fcf1255e32e913b",
+    "url": "/movie_app/static/js/2.94bb94b3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.94bb94b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2e9f07d70cdf6934d86",
+    "url": "/movie_app/static/js/main.39843ccf.chunk.js"
+  },
+  {
+    "revision": "b03db121bc0725d553bd",
+    "url": "/movie_app/static/js/runtime-main.4970d62d.js"
+  }
+]);
